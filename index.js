7cd50ae1dc451aa30ff7ca516236d1edb4386334
Jack Le Hamster <jacklehamster@gmail.com>
2022-12-04 22:33:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function setOutput(key, value) {
+  // Temporary hack until core actions library catches up with github new recommendations
+  const output = process.env['GITHUB_OUTPUT']
+  fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
