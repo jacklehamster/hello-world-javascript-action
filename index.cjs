79bc87e7a30957fbe811c837f544355ab08813a0
Jack Le Hamster <jacklehamster@gmail.com>
2024-10-13 20:41:38 -0700

    Fix dates.

--- index.cjs
+++ index.cjs
@@ -1,18 +1,9 @@
-// import { createRequire } from 'module';
-// const require = createRequire(import.meta.url);
-
-const core = require('@actions/core');
-const github = require('@actions/github');
-// const {saveDirectoryStructure} = require('@dobuki/recurse-directory');
+const core = require("@actions/core");
 const os = require("os");
 const fs = require("fs");
 const md5 = require("md5");
-// import core from '@actions/core';
-// // import {saveDirectoryStructure} from '@dobuki/recurse-directory';
-// import os from 'os';
-// import fs from 'fs';
 const stringify = require("json-stable-stringify");
-
+const { execSync } = require("child_process");
 
 async function recursePath(path, callback, options) {
   if (options?.ignore) {
@@ -28,116 +19,127 @@ async function recursePath(path, callback, options) {
     return;
   }
 
-
-
   const fileList = await fs.promises.readdir(path);
 
-  return Promise.all(fileList.map(fileName => `${path}/${fileName}`)
-    .map(async filePath => recursePath(filePath, callback, options)));
+  return Promise.all(
+    fileList
+      .map((fileName) => `${path}/${fileName}`)
+      .map(async (filePath) => recursePath(filePath, callback, options))
+  );
 }
 
-function insertPathInStructure(pathSplit, index, structure) {
-  const isFile = index === pathSplit.length - 1;
-  if (isFile) {
-    if (!structure.files) {
-      structure.files = {};
-    }
-    const { mtime } = fs.statSync(pathSplit.join("/"));
-    structure.files[pathSplit[index]] = mtime.getTime();
-  } else {
-    if (!structure.dir) {
-      structure.dir = {};
-    }
-    const subStructure = structure.dir[pathSplit[index]] = (structure.dir[pathSplit[index]] ?? {});
-    insertPathInStructure(pathSplit, index + 1, subStructure);
+function getGitCommitTime(filePath) {
+  try {
+    const command = `git log -1 --format=%ct -- ${filePath}`;
+    const commitTime = execSync(command).toString().trim();
+    return new Date(commitTime * 1000); // Convert Unix timestamp to JavaScript Date object
+  } catch (error) {
+    console.error(`Error getting git commit time for ${filePath}:`, error);
+    return null;
   }
 }
 
-async function getDirectoryStructure(rootPath, { ignore, cutoff = 0 }, extension) {
+async function getDirectoryStructure(
+  rootPath,
+  { ignore, cutoff = 0 },
+  extension
+) {
   const root = {};
-  await recursePath(rootPath, async path => {
-    if (extension && !path.endsWith(extension)) {
-      return;
+  await recursePath(
+    rootPath,
+    async (path) => {
+      if (extension && !path.endsWith(extension)) {
+        return;
+      }
+      root[path.split("/").slice(cutoff).join("/")] =
+        getGitCommitTime(path) ?? "untracked"; //fs.statSync(path).mtime;
+    },
+    {
+      ignore,
     }
-    root[path.split("/").slice(cutoff).join("/")] = fs.statSync(path).mtime;
-//    insertPathInStructure(path.split("/"), cutoff, root);
-  }, {
-    ignore,
-  });
+  );
   return root;
 }
 
-async function saveDirectoryStructure(path, target, { ignore, cutoff, space }, extension) {
-  const structure = await getDirectoryStructure(path, { ignore, cutoff }, extension);
+async function saveDirectoryStructure(
+  path,
+  target,
+  { ignore, cutoff, space },
+  extension
+) {
+  const structure = await getDirectoryStructure(
+    path,
+    { ignore, cutoff },
+    extension
+  );
   const md5Hash = md5(stringify(structure));
   structure.md5 = md5Hash;
   const json = stringify(structure, { space });
-  
+
   await fs.promises.writeFile(target, json);
 }
 
-
-
-
 function setOutput(key, value) {
   // Temporary hack until core actions library catches up with github new recommendations
-  const output = process.env['GITHUB_OUTPUT']
-  fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
+  const output = process.env["GITHUB_OUTPUT"];
+  if (output) {
+    fs.appendFileSync(output, `${key}=${value}${os.EOL}`);
+  }
 }
 
 try {
-  // const file = fs.readFileSync(core.getInput('file'));
-  // const json = JSON.parse(file);
-
-
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // console.log(process.cwd());
-  // const { mtime, ctime } = fs.statSync(core.getInput('file'))
-  
-  // json.hello = `Hello ${nameToGreet}`;
-  // json.mtime = mtime;
-  // json.ctime = ctime;
-
-  // fs.writeFileSync(core.getInput('file'), JSON.stringify(json, null, '   '));
-  
-
   async function execute() {
     const directories = fs.readdirSync(".");
     console.log(directories);
-    
-    const ignore = ['.git', 'node_modules'];
-    
+
+    const ignore = [".git", "node_modules"];
+
     const promises = directories.map(async (dir) => {
       if (!fs.statSync(dir).isDirectory()) {
         return;
       }
-      if (ignore.some(i => dir.startsWith(i))) {
+      if (ignore.some((i) => dir.startsWith(i))) {
         return;
       }
-      await saveDirectoryStructure(dir, `${dir}/dir.json`, { ignore: [...ignore, `${dir}/dir.json`, `${dir}/dir-json.json`], cutoff: 1, space: "  " })
-      .then(() => {
-        const content = fs.readFileSync(`${dir}/dir.json`, { encoding: "utf8" });
-        console.info(content);
-      });
-      await saveDirectoryStructure(dir, `${dir}/dir-json.json`, { ignore: [...ignore, `${dir}/dir.json`, `${dir}/dir-json.json`], cutoff: 1, space: "  " }, ".json")
-      .then(() => {
-        const content = fs.readFileSync(`${dir}/dir-json.json`, { encoding: "utf8" });
-        console.info(content);
-      });
+      const ignoreList = [...ignore, `${dir}/dir.json`, `${dir}/dir-json.json`];
+      const dirStructureConfig = {
+        ignore: ignoreList,
+        cutoff: 1,
+        space: "  ",
+      };
+      await Promise.all([
+        saveDirectoryStructure(dir, `${dir}/dir.json`, dirStructureConfig).then(
+          () => {
+            const content = fs.readFileSync(`${dir}/dir.json`, {
+              encoding: "utf8",
+            });
+            console.info(content);
+          }
+        ),
+        saveDirectoryStructure(
+          dir,
+          `${dir}/dir-json.json`,
+          dirStructureConfig,
+          ".json"
+        ).then(() => {
+          const content = fs.readFileSync(`${dir}/dir-json.json`, {
+            encoding: "utf8",
+          });
+          console.info(content);
+        }),
+      ]);
     });
-  
-    for (promise of promises) {
+
+    for (const promise of promises) {
       await promise;
     }
   }
 
   execute().then(() => {
-    const time = (new Date()).getTime();
-    setOutput("time", time);  
+    const time = new Date().getTime();
+    setOutput("time", time);
   });
-  
+
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
